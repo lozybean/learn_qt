bf39dfe7677ee9fc8eedb1169990772550089ec5
lyon <lozybean@163.com>
2016-05-31 09:57:33 +0800

    use QTableWidget

--- search_exon/query_exon_info.py
+++ search_exon/query_exon_info.py
@@ -27,12 +27,27 @@ class MyWindow(QMainWindow, Ui_MainWindow):
 
     def show_search_result(self):
         gene_name = self.lineEdit.text()
-        search_result = self.search(gene_name)
-        self.textBrowser.setText(search_result)
+        search_result = list(self.search(gene_name))
+        self.init_table(4, len(search_result))
+        for index, value in enumerate(search_result):
+            (gene_name, transcript_id, exon_length, exon_num) = value
+            self.tableWidget.setItem(index, 0, QTableWidgetItem(gene_name))
+            self.tableWidget.setItem(index, 1, QTableWidgetItem(transcript_id))
+            self.tableWidget.setItem(index, 2, QTableWidgetItem(str(exon_length)))
+            self.tableWidget.setItem(index, 3, QTableWidgetItem(str(exon_num)))
+
+    def init_table(self, col, row):
+        self.tableWidget.setColumnCount(col)
+        self.tableWidget.setRowCount(row)
+        self.tableWidget.setHorizontalHeaderLabels(['gene_name', 'transcript_id',
+                                                    'exon_length', 'exon_num'])
+        self.tableWidget.setColumnWidth(0, 100)
+        self.tableWidget.setColumnWidth(1, 200)
+        self.tableWidget.setColumnWidth(2, 100)
+        self.tableWidget.setColumnWidth(3, 100)
 
     def search(self, gene_name):
         cursor_mRNA = mRNA_info.find({'gene': gene_name}, {'Name': 1, '_id': 0})
-        string = ''
         for q in cursor_mRNA:
             mRNA = q['Name']
             exon_length = 0
@@ -42,8 +57,7 @@ class MyWindow(QMainWindow, Ui_MainWindow):
             for q in cursor_exon:
                 exon_length += q['end'] - q['start'] + 1
                 exon_num += 1
-            string += '{gene_name}\t{mRNA}\t{exon_length}\t{exon_num}\n'.format_map(vars())
-        return string
+            yield gene_name, mRNA, exon_length, exon_num
 
 
 if __name__ == '__main__':
